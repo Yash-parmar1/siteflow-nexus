05785d3948af1356b224ae26d28d1658073ccbff
Yash-parmar1 <fireryleaf@gmail.com>
2026-02-11 16:35:55 +0530

    ip

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.41:8080/api',
+  baseURL: 'http://192.168.29.124:8080/api',
 });
 
 api.interceptors.request.use(
